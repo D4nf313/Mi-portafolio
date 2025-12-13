c62ce49bdadd2629eebd73a6f93f364584015b2b
Daniel Suarez <danfecisf@gmail.com>
2025-12-13 12:33:35 -0500

    Primera version portafolio estructura basica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'portafolio-daniel-suarez-full-stack',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component')
+            .then(m => m.HomeComponent)
+      },
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./pages/about/about.component')
+            .then(m => m.AboutComponent)
+      },
+      {
+        path: 'projects',
+        loadComponent: () =>
+          import('./pages/projects/projects.component')
+            .then(m => m.ProjectsComponent)
+      },
+      {
+        path: 'contact',
+        loadComponent: () =>
+          import('./pages/contact/contact.component')
+            .then(m => m.ContactComponent)
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'portafolio-daniel-suarez-full-stack',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'portafolio-daniel-suarez-full-stack'
+  }
+];
+
